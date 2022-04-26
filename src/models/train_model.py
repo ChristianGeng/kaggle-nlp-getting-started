c758ffc2e779906ae94f9f8f4d6f4056ce52b359
ChristianGeng <christian.c.geng@gmail.com>
2022-04-26 18:24:28 +0200

    add src

---
+++ src/models/train_model.py
@@ -0,0 +1,15 @@
+
+# from . import get_project_root
+
+#from src import get_project_root
+
+
+PROJECT_ROOT = get_project_root()
+
+
+
+def main():
+    breakpoint()
+
+if __name__ == '__main__':
+   main()
