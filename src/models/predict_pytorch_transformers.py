d1365cc60d124a79c7573b3bf6e439e3a91a83e8
Christian Geng <cgeng@audeering.com>
2022-05-04 16:37:34 +0200

    Transfomer Model Vanilla.

--- src/models/predict_pytorch_transformers.py
+++ src/models/predict_pytorch_transformers.py
@@ -1,14 +1,10 @@
-from transformers import BertTokenizer
-
-MODEL_NAME = "pytorch_transformers"
-
-
-from models.train_pytorch_transformers import BertClassifier
-from models.train_pytorch_transformers import Dataset
-
 from utils.utils import get_project_root
 from utils.utils import get_test_df
 from utils.utils import get_train_df
+
+import pandas as pd
+
+import logging
 
 import audeer
 
@@ -18,11 +14,34 @@ import torch
 
 MODEL_NAME = "pytorch_transformers"
 
+epoch_num = 1
+
+audeer.mkdir(os.path.join(get_project_root(), "submissions"))
+submission_fname = os.path.join(
+    get_project_root(),
+    "submissions",
+    "submission_" + "_" + str(epoch_num + 1).zfill(4) + "_" + MODEL_NAME + ".csv",
+)
+
+from transformers import BertTokenizer
+from models.train_pytorch_transformers import BertClassifier
+from models.train_pytorch_transformers import Dataset
+
+
+
+# Epochs: 1 | Train Loss:  0.298                 | Train Accuracy:  0.680                 | Val Loss:  0.250                 | Val Accuracy:  0.779
+# Epochs: 2 | Train Loss:  0.206                 | Train Accuracy:  0.833                 | Val Loss:  0.231                 | Val Accuracy:  0.798
+# Epochs: 3 | Train Loss:  0.169                 | Train Accuracy:  0.875                 | Val Loss:  0.236                 | Val Accuracy:  0.808
+# Epochs: 4 | Train Loss:  0.139                 | Train Accuracy:  0.905                 | Val Loss:  0.251                 | Val Accuracy:  0.798
+# Epochs: 5 | Train Loss:  0.113                 | Train Accuracy:  0.928                 | Val Loss:  0.266                 | Val Accuracy:  0.799
+
 MODEL_DIR = os.path.join(get_project_root(), "models")
 MODEL_DIR = audeer.safe_path(MODEL_DIR)
 
-epoch_num = 0
-ckpt_fname = os.path.join(MODEL_DIR, MODEL_NAME + '_' +str(epoch_num).zfill(4) +  ".pth")
+
+ckpt_fname = os.path.join(
+    MODEL_DIR, MODEL_NAME + "_" + str(epoch_num).zfill(4) + ".pth"
+)
 
 
 if not os.path.exists(ckpt_fname):
@@ -30,19 +49,33 @@ if not os.path.exists(ckpt_fname):
 
 checkpoint = torch.load(ckpt_fname)
 model = BertClassifier()
-model.load_state_dict(checkpoint['model'])
+model.load_state_dict(checkpoint["model"])
 
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
+
+
+
+logger = logging.getLogger(__name__)
+logger.info("making final data set from raw data")
+log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.INFO, format=log_fmt)
+
 
 
 class DatasetTest(torch.utils.data.Dataset):
-
     def __init__(self, df):
 
-        self.texts = [tokenizer(text,
-                               padding='max_length', max_length = 512, truncation=True,
-                                return_tensors="pt") for text in df['text']]
+        self.texts = [
+            tokenizer(
+                text,
+                padding="max_length",
+                max_length=512,
+                truncation=True,
+                return_tensors="pt",
+            )
+            for text in df["text"]
+        ]
 
     def __len__(self):
         return len(self.texts)
@@ -60,11 +93,13 @@ class DatasetTest(torch.utils.data.Dataset):
 
 
 test_data = get_test_df()
+
 test = DatasetTest(test_data)
 
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
+# device = "cpu"
 
 if use_cuda:
     model = model.cuda()
@@ -72,41 +107,51 @@ if use_cuda:
 total_acc_test = 0
 
 
-test_dataloader = torch.utils.data.DataLoader(test, batch_size=1)
+test_dataloader = torch.utils.data.DataLoader(test, batch_size=1, shuffle = False)
+
+# for batch_idx, (data, target) in enumerate(test_data_loader): print(batch_idx)
 
 
 results = []
 
 idx = 0
 
+import numpy as np
+
+# breakpoint()
+
+# model.eval()
+
 with torch.no_grad():
     # returns no label
     for test_input in test_dataloader:
 
         # test_label = test_label.to(device)
-        mask = test_input['attention_mask'].to(device)
-        input_id = test_input['input_ids'].squeeze(1).to(device)
+        mask = test_input["attention_mask"].to(device)
+
+        input_id = test_input["input_ids"].squeeze(1).to(device)
 
         output = model(input_id, mask)
-
         result = output.argmax(dim=1)
+        result = result.cpu().detach().numpy().flatten()[0]
+        # results += list(result.cpu().detach().numpy().flatten())
+        # result.cpu().detach().numpy()
+        # print(f"result now has {len(results)} entries")
         results.append(result)
-        # print(f"processing {idx} out of {test_data.shape[0]}")
+        print(f"processing {idx} out of {test_data.shape[0]}")
         idx += 1
         # int(output.argmax(dim=1))
         # acc = (output.argmax(dim=1) == test_label).sum().item()
         # total_acc_test += acc
-breakpoint()
+
 
 
 predictions = [int(x) for x in results]
 
-import pandas as pd
+
+
 submission_df = pd.DataFrame({"id": test_data["id"], "target": predictions})
 # submission = pd.DataFrame(submission_df)
-
-audeer.mkdir(os.path.join(get_project_root(), "submissions"))
-submission_fname = os.path.join(get_project_root(), "submissions", "submission_" + MODEL_NAME + ".csv")
 
 logger.info(f"writing submissions to {submission_fname}.")
 submission_df.to_csv(submission_fname, index=False)
--- src/models/train_pytorch_transformers.py
+++ src/models/train_pytorch_transformers.py
@@ -31,12 +31,14 @@ from transformers import BertTokenizer
 PROJECT_ROOT = get_project_root()
 MODEL_NAME = "pytorch_transformers"
 
+MODEL_DIR = os.path.join(get_project_root(), "models")
+os.makedirs(MODEL_DIR, exist_ok=True)
+
 
 logger = logging.getLogger(__name__)
 logger.info("making final data set from raw data")
 log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-logging.basicConfig(level=logging.DEBUG, format=log_fmt)
-
+logging.basicConfig(level=logging.INFO, format=log_fmt)
 
 
 def clean(text):
@@ -51,19 +53,17 @@ def clean(text):
     return res.strip()
 
 
-
 # If you have datasets from different languages,
 # you might want to use bert-base-multilingual-cased.
 # Specifically, if your dataset is in German, Dutch, Chinese, Japanese, or Finnish,
 # you might want to use a tokenizer pre-trained specifically in these languages.
 # You can check the name of the corresponding pre-trained tokenizer at https://huggingface.co/models
-tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
-
+tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
 
 
 def explain_tokenizer():
 
-    example_text = 'I will watch Memento tonight'
+    example_text = "I will watch Memento tonight"
 
     # padding : to pad each sequence to the maximum length that you specify.
     # max_length : the maximum length of each sequence.
@@ -73,11 +73,13 @@ def explain_tokenizer():
     # length will be truncated.
     # return_tensors : the type of tensors that will be returned.
     # Since we’re using Pytorch, then we use pt. If you use Tensorflow, then you need to use tf .
-    bert_input = tokenizer(example_text,
-                           padding='max_length',
-                           max_length = 10,
-                           truncation=True,
-                           return_tensors="pt")
+    bert_input = tokenizer(
+        example_text,
+        padding="max_length",
+        max_length=10,
+        truncation=True,
+        return_tensors="pt",
+    )
 
     # tokenizer.decode(bert_input.input_ids[0])
     # [CLS] Our Deeds are the Reason of this [SEP]
@@ -92,19 +94,23 @@ def explain_tokenizer():
     # If we only have a single sequence, then all of the token type ids will be 0
 
 
-
-
 class Dataset(torch.utils.data.Dataset):
-    """Dataset class that will serve as a class to generate data.
-    """
+    """Dataset class that will serve as a class to generate data."""
 
     def __init__(self, df):
 
         # self.labels = [labels[label] for label in df['category']]
-        self.labels = df['target'].values
-        self.texts = [tokenizer(text,
-                               padding='max_length', max_length = 512, truncation=True,
-                                return_tensors="pt") for text in df['text']]
+        self.labels = df["target"].values
+        self.texts = [
+            tokenizer(
+                text,
+                padding="max_length",
+                max_length=512,
+                truncation=True,
+                return_tensors="pt",
+            )
+            for text in df["text"]
+        ]
 
     def classes(self):
         return self.labels
@@ -129,19 +135,17 @@ class Dataset(torch.utils.data.Dataset):
 
 
 class BertClassifier(nn.Module):
-    """Model using a pre-trained BERT base model which has 12 layers of Transformer encoder.
-    """
+    """Model using a pre-trained BERT base model which has 12 layers of Transformer encoder."""
 
     def __init__(self, dropout=0.5):
 
         super(BertClassifier, self).__init__()
 
-
         # If your dataset is not in English, it would be best if you use bert-base-multilingual-cased model.
         # If your data is in German, Dutch, Chinese, Japanese, or Finnish,
         # you can use the model pre-trained specifically in these languages.
         # You can check the name of the corresponding pre-trained model
-        self.bert = BertModel.from_pretrained('bert-base-cased')
+        self.bert = BertModel.from_pretrained("bert-base-cased")
         self.dropout = nn.Dropout(dropout)
         # At the end of the linear layer, we have a vector of size n categories,
         # each corresponds to a category of our labels
@@ -153,7 +157,9 @@ class BertClassifier(nn.Module):
         # _ contains the embedding vectors of all of the tokens in a sequence.
         # pooled_output, contains the embedding vector of [CLS] token.
         # For a text classification task, it is enough to use this embedding as an input for our classifier.
-        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        _, pooled_output = self.bert(
+            input_ids=input_id, attention_mask=mask, return_dict=False
+        )
         dropout_output = self.dropout(pooled_output)
         linear_output = self.linear(dropout_output)
         final_layer = self.relu(linear_output)
@@ -172,84 +178,87 @@ def train(model, train_data, val_data, learning_rate, epochs):
     device = torch.device("cuda" if use_cuda else "cpu")
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = Adam(model.parameters(), lr= learning_rate)
+    optimizer = Adam(model.parameters(), lr=learning_rate)
 
     if use_cuda:
 
-            model = model.cuda()
-            criterion = criterion.cuda()
+        model = model.cuda()
+        criterion = criterion.cuda()
 
     for epoch_num in range(epochs):
 
-            total_acc_train = 0
-            total_loss_train = 0
+        total_acc_train = 0
+        total_loss_train = 0
 
-            for train_input, train_label in tqdm(train_dataloader):
+        for train_input, train_label in tqdm(train_dataloader):
 
-                train_label = train_label.to(device)
-                mask = train_input['attention_mask'].to(device)
-                input_id = train_input['input_ids'].squeeze(1).to(device)
+            train_label = train_label.to(device)
+            mask = train_input["attention_mask"].to(device)
+            input_id = train_input["input_ids"].squeeze(1).to(device)
+
+            output = model(input_id, mask)
+
+            batch_loss = criterion(output, train_label)
+            total_loss_train += batch_loss.item()
+
+            acc = (output.argmax(dim=1) == train_label).sum().item()
+            total_acc_train += acc
+
+            model.zero_grad()
+            batch_loss.backward()
+            optimizer.step()
+
+        total_acc_val = 0
+        total_loss_val = 0
+
+        with torch.no_grad():
+
+            for val_input, val_label in val_dataloader:
+
+                val_label = val_label.to(device)
+                mask = val_input["attention_mask"].to(device)
+                input_id = val_input["input_ids"].squeeze(1).to(device)
 
                 output = model(input_id, mask)
 
-                batch_loss = criterion(output, train_label)
-                total_loss_train += batch_loss.item()
+                batch_loss = criterion(output, val_label)
+                total_loss_val += batch_loss.item()
 
-                acc = (output.argmax(dim=1) == train_label).sum().item()
-                total_acc_train += acc
+                acc = (output.argmax(dim=1) == val_label).sum().item()
+                total_acc_val += acc
 
-                model.zero_grad()
-                batch_loss.backward()
-                optimizer.step()
-
-            total_acc_val = 0
-            total_loss_val = 0
-
-            with torch.no_grad():
-
-                for val_input, val_label in val_dataloader:
-
-                    val_label = val_label.to(device)
-                    mask = val_input['attention_mask'].to(device)
-                    input_id = val_input['input_ids'].squeeze(1).to(device)
-
-                    output = model(input_id, mask)
-
-                    batch_loss = criterion(output, val_label)
-                    total_loss_val += batch_loss.item()
-
-                    acc = (output.argmax(dim=1) == val_label).sum().item()
-                    total_acc_val += acc
-
-            print(
-                f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
+        print(
+            f"Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
                 | Train Accuracy: {total_acc_train / len(train_data): .3f} \
                 | Val Loss: {total_loss_val / len(val_data): .3f} \
-                | Val Accuracy: {total_acc_val / len(val_data): .3f}')
+                | Val Accuracy: {total_acc_val / len(val_data): .3f}"
+        )
 
-            checkpoint = dict()
-            checkpoint["model"] = model.state_dict()
-            checkpoint["optimizer"] = optimizer.state_dict()
-            checkpoint["epoch"] = epoch_num
-            checkpoint["train_loss"] = total_loss_train / len(train_data)
-            checkpoint["train_acc"] = total_acc_train / len(train_data)
-            checkpoint["val_loss"] = total_loss_val / len(val_data)
-            checkpoint["val_acc"] = total_acc_val / len(val_data)
-            os.path.join(PROJECT_ROOT)
-            MODEL_DIR = os.path.join(get_project_root(), "models")
-            os.makedirs(MODEL_DIR)
-            ckpt_fname = os.path.join(MODEL_DIR, MODEL_NAME + '_' +str(epoch_num).zfill(4) +  ".pth")
-            torch.save(checkpoint, ckpt_fname)
+        checkpoint = dict()
+        checkpoint["model"] = model.state_dict()
+        checkpoint["optimizer"] = optimizer.state_dict()
+        checkpoint["epoch"] = epoch_num
+        checkpoint["train_loss"] = total_loss_train / len(train_data)
+        checkpoint["train_acc"] = total_acc_train / len(train_data)
+        checkpoint["val_loss"] = total_loss_val / len(val_data)
+        checkpoint["val_acc"] = total_acc_val / len(val_data)
+        os.path.join(PROJECT_ROOT)
+        ckpt_fname = os.path.join(
+            MODEL_DIR, MODEL_NAME + "_" + str(epoch_num).zfill(4) + ".pth"
+        )
+        logger.info(f"Writing checkpoiont to {ckpt_fname}")
+        torch.save(checkpoint, ckpt_fname)
+
 
 def main():
 
-
     df = get_train_df()
     np.random.seed(112)
-    df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=42),
-                                        [int(.8*len(df)), int(.9*len(df))])
+    df_train, df_val, df_test = np.split(
+        df.sample(frac=1, random_state=42), [int(0.8 * len(df)), int(0.9 * len(df))]
+    )
 
-    print(len(df_train),len(df_val), len(df_test))
+    print(len(df_train), len(df_val), len(df_test))
     EPOCHS = 5
     model = BertClassifier()
 
